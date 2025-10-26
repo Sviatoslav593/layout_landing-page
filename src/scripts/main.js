ed5d10342c4ddd2f2d80077b025123e8dcd36353
SviatoslavPotapenko <Sviatoslav.Potapenko@lnu.edu.ua>
2025-10-26 16:14:35 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,7 @@
 const body = document.querySelector('.page__body');
 const menuOpen = document.querySelector('.icon--menu');
 const menuClose = document.querySelector('.icon--close');
+const scrollActivators = document.querySelectorAll('.scroll--activator');
 
 menuOpen.addEventListener('click', () => {
   body.classList.add('page__body--no-scroll');
@@ -8,4 +9,10 @@ menuOpen.addEventListener('click', () => {
 
 menuClose.addEventListener('click', () => {
   body.classList.remove('page__body--no-scroll');
+});
+
+scrollActivators.forEach((link) => {
+  link.addEventListener('click', () => {
+    body.classList.remove('page__body--no-scroll');
+  });
 });
